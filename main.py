f466bbd7f62960a21ee6dd6bf29f2f0435374cc7
Maestrox <91748131+maestroxdenis@users.noreply.github.com>
2025-04-07 23:00:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ from aiogram.types.chat_member_member import ChatMemberMember
 
 # Define connection parameters
 print("here is database server:")
-print(${{shared.DATABASE_SERVER}})
+print(os.getenv("shared.DATABASE_SERVER"))
 server = str(os.getenv("DATABASE_SERVER"))
 db_username = str(os.getenv("DATABASE_USER"))
 password = str(os.getenv("DATABASE_PASSWORD"))
